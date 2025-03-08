0879bf6e60fc05d21e1882292d120ea46cbc6039
Diego M <diemoret17@gmail.com>
2025-03-08 15:28:27 -0600

    production final

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
 import Login from './views/LoginPage/LoginPage';
 import MainLayout from './layouts/MainLayout';
-import Sidebar from './components/Sidebar';
 import Register from './views/RegisterPage/RegisterPage';
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<MainLayout />} />
-        <Route path="/register" element={<Register />} />
-        
+        <Route path="/register" element={<Register />} />        
       </Routes>
     </Router>
   );
